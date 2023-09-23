12abfb2b0a80c6123107f9ad4670962d55b97e07
AariyaTharshan <145669051+ATGprofessional@users.noreply.github.com>
2023-09-23 15:13:28 +0530

    ASSIGNMENT-2

--- assign2.js
+++ assign2.js
@@ -6,5 +6,14 @@ app.get('/',(req,res)=>{
         res.send(Note)   
    }
 )
-
+app.get('/notes/:id',(req,res) => {
+    const id = req.params.id;
+    for (let i = 0; i < NOTES.length; i++){
+        if(NOTES[i].id==id){
+            res.send(NOTES[i]);
+        }
+    }
+    res.send("NOTES not found");
+ }
+)
 app.listen(3030)
